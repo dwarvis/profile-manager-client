8b59e3d55c49fed9787c35aae8eb025fae7929fa
dwarvis <davischeng77@gmail.com>
2020-01-10 10:23:43 +0800

    changed ngrok link

--- app/src/main/java/edu/cis/clientapp/Model/Constants.java
+++ app/src/main/java/edu/cis/clientapp/Model/Constants.java
@@ -1,5 +1,5 @@
 package edu.cis.clientapp.Model;
 
 public class Constants {
-    public static final String HOST = "NGROK_URL_HERE";
+    public static final String HOST = "http://9458b04b.ngrok.io";
 }
